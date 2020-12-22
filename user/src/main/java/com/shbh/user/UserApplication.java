4d3590ea62728eb330648c97f8efb5f76de037ab
MaZhuli <345957335@qq.com>
2020-12-22 14:04:38 +0800

    熔断降级,修改boot和cloud版本

--- user/src/main/java/com/shbh/user/UserApplication.java
+++ user/src/main/java/com/shbh/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.shbh.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrixDashboard
+@EnableCircuitBreaker
 public class UserApplication {
 
     public static void main(String[] args) {
